e0ad4e94bb272dd4c35861c7467f79d3d0e2f83b
Gustavo <gustavo.ribeiro@sc.senai.br>
2022-08-06 13:45:27 -0300

    first commit

---
+++ src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
@@ -0,0 +1,16 @@
+package br.com.alura.loja.desconto;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class CalculadoraDeDescontos {
+
+    public BigDecimal calcular(Orcamento orcamento) {
+        Desconto cadeiaDeDescontos = new DescontoOrcamentoMais5Itens(
+                new DescontoOrcamentoValorMaiorQuinhentos(
+                        new SemDesconto()));
+
+        return cadeiaDeDescontos.calcular(orcamento);
+    }
+}
