85b11d8cb55c4b30dc32163fbbf3983bbcccaa5b
Gustavo <gustavo.ribeiro@sc.senai.br>
2022-08-08 11:17:33 -0300

    implemented design pattern STATE

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,17 +1,43 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeDeItens;
+    private SituacaoOrcamento situacao;
 
 
 
     public Orcamento(BigDecimal valor, int quantidadeDeItens) {
         this.valor = valor;
         this.quantidadeDeItens = quantidadeDeItens;
+        this.situacao = new EmAnalise();
+    }
+
+    // State
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    // State
+    public void aprovar() {
+        this.situacao.aprovar(this);
+    }
+
+    // State
+    public void reprovar() {
+        this.situacao.reprovar(this);
+    }
+
+    // State
+    public void finalizar() {
+        this.situacao.finalizar(this);
     }
 
     public BigDecimal getValor() {
@@ -20,5 +46,13 @@ public class Orcamento {
 
     public int getQuantidadeDeItens() {
         return quantidadeDeItens;
+    }
+
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
     }
 }
---
+++ src/br/com/alura/loja/orcamento/situacao/Finalizado.java
@@ -0,0 +1,9 @@
+package br.com.alura.loja.orcamento.situacao;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+// State
+public class Finalizado extends SituacaoOrcamento {
+}
